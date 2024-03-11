3977ad2ebf2cbff4333524d0446b2e0bee92ee59
Skiftsu <skiftsu@gmail.com>
2024-03-11 14:11:09 +0500

    flags_config: corrected variables use

--- templates/yandex-music.sh
+++ templates/yandex-music.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-~/.config}
-CONFIG_FILE_NAME=yandex-music-flags.conf
+XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+CONFIG_FILE_NAME="yandex-music-flags.conf"
 
 if [[ -f $XDG_CONFIG_HOME/$CONFIG_FILE_NAME ]]; then
-   YANDEX_MUSIC_USER_FLAGS="$(sed 's/#.*//' $XDG_CONFIG_HOME/$CONFIG_FILE_NAME | tr '\n' ' ')"
+   YANDEX_MUSIC_USER_FLAGS="$(sed 's/#.*//' ${XDG_CONFIG_HOME}/${CONFIG_FILE_NAME} | tr '\n' ' ')"
    echo "User flags:" "${YANDEX_MUSIC_USER_FLAGS[@]}"
 fi
 
 # Launch
-exec electron28 /usr/lib/yandex-music/yandex-music.asar $YANDEX_MUSIC_USER_FLAGS "$@" 
+exec electron28 /usr/lib/yandex-music/yandex-music.asar ${YANDEX_MUSIC_USER_FLAGS} "$@" 
