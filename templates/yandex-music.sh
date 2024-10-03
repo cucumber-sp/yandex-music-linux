4d8753bf03e9faf68ec0fd716c0a55450848e5cf
Andrey Onishchenko <loraner123@gmail.com>
2024-10-03 19:35:48 +0300

    Update executable for wayland

--- templates/yandex-music.sh
+++ templates/yandex-music.sh
@@ -17,12 +17,13 @@ source "$CONFIG_FILE"
 
 WAYLAND_FLAGS=""
 if [ "$XDG_SESSION_TYPE" == "wayland" ]; then
-    WAYLAND_FLAGS="--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --ozone-platform=wayland"
+    WAYLAND_FLAGS="--ozone-platform=wayland"
 fi
 
 ELECTRON_BIN=${ELECTRON_CUSTOM_BIN:-%electron_path%}
 
 export TRAY_ENABLED=${TRAY_ENABLED:-0}
 export DEV_TOOLS=${DEV_TOOLS:-0}
+export CUSTOM_TITLE_BAR=${CUSTOM_TITLE_BAR:-0}
 
 exec "${ELECTRON_BIN}" "%asar_path%" $ELECTRON_ARGS $WAYLAND_FLAGS
